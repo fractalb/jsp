c9ca761b34ebefe11b390c13a42329c1a139d695
Balaji Marisetti <balajimarisetti@gmail.com>
2026-01-30 19:26:53 +0530

    Changed Option<> return types to Result<> types

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,27 @@ pub enum Number {
     Float(f64),
 }
 
+#[derive(Debug, PartialEq)]
+pub enum JspError {
+    Empty,
+    Eof,
+    HasTail,
+    InvalidArray,
+    InvalidBool,
+    InvalidEscapeSequence,
+    InvalidNull,
+    InvalidNumber,
+    InvalidObject,
+    InvalidString,
+    InvalidValue,
+    MissingArrayEnd,
+    MissingArrayStart,
+    MissingColon,
+    MissingObjectEnd,
+    MissingObjectStart,
+    NoPair,
+}
+
 pub fn consume_char(p: &mut PkChars, c: char) -> bool {
     if p.next_if_eq(&c).is_some() {
         true
@@ -40,153 +61,160 @@ pub fn consume_prefix(p: &mut PkChars, t: &mut PkChars) -> String {
     prefix
 }
 
-fn consume_key_pair(p: &mut PkChars) -> Option<(String, JsonValue)> {
-    let p_clone = p.clone();
+fn consume_key_pair(p: &mut PkChars) -> Result<(String, JsonValue), JspError> {
     jsp_consume_whitespace(p);
-    if let Some(key) = jsp_consume_string(p) {
-        jsp_consume_whitespace(p);
-        if consume_char(p, ':') {
-            jsp_consume_whitespace(p);
-            if let Some(val) = jsp_consume_value(p) {
-                return Some((key, val));
-            }
-        }
+    if Some(&'}') == p.peek() {
+        return Err(JspError::NoPair);
     }
-    p.clone_from(&p_clone);
-    None
+    let key = jsp_consume_string(p)?;
+    jsp_consume_whitespace(p);
+    if consume_char(p, ':') {
+        jsp_consume_whitespace(p);
+        let val = jsp_consume_value(p)?;
+        Ok((key, val))
+    } else {
+        Err(JspError::MissingColon)
+    }
 }
 
 fn consume_char_sequence(p: &mut PkChars, seq: &str) -> bool {
-    let p_clone = p.clone();
     for c in seq.chars() {
         if !consume_char(p, c) {
-            p.clone_from(&p_clone);
             return false;
         }
     }
     true
 }
 
-pub fn jsp_consume_array(p: &mut PkChars) -> Option<Vec<JsonValue>> {
-    let p_clone = p.clone();
+pub fn jsp_consume_array(p: &mut PkChars) -> Result<Vec<JsonValue>, JspError> {
     let mut jsa = Vec::<JsonValue>::new();
     if !consume_char(p, '[') {
-        p.clone_from(&p_clone);
-        return None;
+        return Err(JspError::MissingArrayStart);
     }
-    if let Some(v) = jsp_consume_value(p) {
-        jsa.push(v);
-        loop {
-            if !consume_char(p, ',') {
-                break;
-            }
-            if let Some(v) = jsp_consume_value(p) {
-                jsa.push(v);
-            } else {
-                p.clone_from(&p_clone);
-                return None;
-            }
+    let val = jsp_consume_value(p);
+    if val == Err(JspError::Empty) {
+        // See if it's an empty array.
+        if consume_char(p, ']') {
+            return Ok(jsa);
+        } else {
+            return Err(JspError::MissingArrayEnd);
         }
     }
-    if !consume_char(p, ']') {
-        p.clone_from(&p_clone);
-        return None;
+    let v = val?;
+    jsa.push(v);
+    loop {
+        if !consume_char(p, ',') {
+            break;
+        }
+        let v = jsp_consume_value(p)?;
+        jsa.push(v);
     }
-    Some(jsa)
+    if consume_char(p, ']') {
+        Ok(jsa)
+    } else {
+        Err(JspError::MissingArrayEnd)
+    }
 }
 
-pub fn jsp_parse_json(s: &str) -> Option<JsonValue> {
+pub fn jsp_parse_json(s: &str) -> Result<JsonValue, JspError> {
     let mut p = s.chars().peekable();
-    let json = jsp_consume_value(&mut p);
-    if p.peek().is_none() { json } else { None }
+    let json = jsp_consume_value(&mut p)?;
+    if p.peek().is_none() {
+        Ok(json)
+    } else {
+        Err(JspError::HasTail)
+    }
 }
 
-pub fn jsp_consume_value(p: &mut PkChars) -> Option<JsonValue> {
-    let p_clone = p.clone();
-    let mut jsonval = None;
+pub fn jsp_consume_value(p: &mut PkChars) -> Result<JsonValue, JspError> {
+    let mut jsonval = Err(JspError::Empty);
     jsp_consume_whitespace(p);
     if let Some(&c) = p.peek() {
-        match c {
+        jsonval = match c {
             '"' => {
-                if let Some(x) = jsp_consume_string(p) {
-                    jsonval = Some(JsonValue::String(x));
-                }
+                let x = jsp_consume_string(p)?;
+                Ok(JsonValue::String(x))
             }
             '{' => {
-                if let Some(x) = jsp_consume_object(p) {
-                    jsonval = Some(JsonValue::Object(x));
-                }
+                let x = jsp_consume_object(p)?;
+                Ok(JsonValue::Object(x))
             }
             '[' => {
-                if let Some(x) = jsp_consume_array(p) {
-                    jsonval = Some(JsonValue::Array(x));
-                }
+                let x = jsp_consume_array(p)?;
+                Ok(JsonValue::Array(x))
             }
             'n' => {
                 if consume_char_sequence(p, "null") {
-                    jsonval = Some(JsonValue::Null);
+                    Ok(JsonValue::Null)
+                } else {
+                    Err(JspError::InvalidNull)
                 }
             }
             't' => {
                 if consume_char_sequence(p, "true") {
-                    jsonval = Some(JsonValue::Bool(true));
+                    Ok(JsonValue::Bool(true))
+                } else {
+                    Err(JspError::InvalidBool)
                 }
             }
             'f' => {
                 if consume_char_sequence(p, "false") {
-                    jsonval = Some(JsonValue::Bool(false));
+                    Ok(JsonValue::Bool(false))
+                } else {
+                    Err(JspError::InvalidBool)
                 }
             }
-            '-' | '0'..='9' => {
-                jsonval = match jsp_consume_number(p) {
-                    Some(Number::Int(x)) => Some(JsonValue::Int(x)),
-                    Some(Number::Float(x)) => Some(JsonValue::Float(x)),
-                    None => None,
-                }
-            }
-            _ => (),
+            '-' | '0'..='9' => match jsp_consume_number(p) {
+                Ok(Number::Int(x)) => Ok(JsonValue::Int(x)),
+                Ok(Number::Float(x)) => Ok(JsonValue::Float(x)),
+                Err(e) => Err(e),
+            },
+            _ => jsonval,
         }
     }
-    if jsonval.is_none() {
-        p.clone_from(&p_clone);
-        None
-    } else {
+    if jsonval.is_ok() {
         jsp_consume_whitespace(p);
-        jsonval
     }
+    jsonval
 }
 
-pub fn jsp_consume_object(mut p: &mut PkChars) -> Option<HashMap<String, JsonValue>> {
-    let p_clone = p.clone();
+pub fn jsp_consume_object(mut p: &mut PkChars) -> Result<HashMap<String, JsonValue>, JspError> {
     let mut m = HashMap::<String, JsonValue>::new();
     if !consume_char(p, '{') {
-        p.clone_from(&p_clone);
-        return None;
+        return Err(JspError::MissingObjectStart);
     }
 
-    if let Some((key, val)) = consume_key_pair(p) {
-        m.insert(key, val);
-        loop {
-            jsp_consume_whitespace(&mut p);
-            if !consume_char(p, ',') {
-                break;
-            }
-            if let Some((key, val)) = consume_key_pair(p) {
-                m.insert(key, val);
-            } else {
-                p.clone_from(&p_clone);
-                return None;
+    match consume_key_pair(p) {
+        Ok((key, val)) => {
+            m.insert(key, val);
+            loop {
+                jsp_consume_whitespace(&mut p);
+                if !consume_char(p, ',') {
+                    break;
+                }
+                match consume_key_pair(p) {
+                    Ok((key, val)) => {
+                        m.insert(key, val);
+                    }
+                    Err(x) => return Err(x),
+                }
             }
         }
-    }
+        Err(JspError::NoPair) => {
+            if !consume_char(p, '}') {
+                panic!("Error: Parsing Bug");
+            }
+            return Ok(m);
+        }
+        Err(e) => return Err(e),
+    };
 
     jsp_consume_whitespace(&mut p);
 
-    if consume_char(p, '}') {
-        Some(m)
+    if !consume_char(p, '}') {
+        Err(JspError::MissingObjectEnd)
     } else {
-        p.clone_from(&p_clone);
-        None
+        Ok(m)
     }
 }
 
@@ -222,23 +250,20 @@ pub fn jsp_consume_hexdigit(p: &mut PkChars) -> Option<u32> {
     None
 }
 
-pub fn jsp_consume_four_hexdigits(p: &mut PkChars) -> Option<u32> {
-    let p_clone = p.clone();
-    let mut val = 0;
+pub fn jsp_consume_four_hexdigits(p: &mut PkChars) -> Option<u16> {
+    let mut val = 0_u16;
     for _i in 0..4 {
         if let Some(v) = jsp_consume_hexdigit(p) {
-            val *= 16;
-            val += v;
+            val *= 16_u16;
+            val += v as u16;
         } else {
-            p.clone_from(&p_clone);
             return None;
         }
     }
     Some(val)
 }
 
-pub fn jsp_consume_number(p: &mut PkChars) -> Option<Number> {
-    let p_clone = p.clone();
+pub fn jsp_consume_number(p: &mut PkChars) -> Result<Number, JspError> {
     let mut nstr = String::new();
     let mut frac = false;
     let mut exp = false;
@@ -252,26 +277,23 @@ pub fn jsp_consume_number(p: &mut PkChars) -> Option<Number> {
         nstr.push('0');
         let c = p.peek();
         if c.is_none() {
-            return Some(Number::Int(0));
+            return Ok(Number::Int(0));
         }
         // Leading zero is not accepted.
         if c.unwrap().is_ascii_digit() {
-            p.clone_from(&p_clone);
-            return None;
+            return Err(JspError::InvalidNumber);
         }
     } else {
         let n: usize = jsp_consume_all_digits(p, &mut nstr);
         if n == 0 {
-            p.clone_from(&p_clone);
-            return None;
+            return Err(JspError::InvalidNumber);
         }
     }
     if consume_char(p, '.') {
         nstr.push('.');
         let n: usize = jsp_consume_all_digits(p, &mut nstr);
         if n == 0 {
-            p.clone_from(&p_clone);
-            return None;
+            return Err(JspError::InvalidNumber);
         }
         frac = true;
     }
@@ -282,140 +304,117 @@ pub fn jsp_consume_number(p: &mut PkChars) -> Option<Number> {
         }
         let n: usize = jsp_consume_all_digits(p, &mut nstr);
         if n == 0 {
-            p.clone_from(&p_clone);
-            return None;
+            return Err(JspError::InvalidNumber);
         }
         exp = true;
     }
 
     if exp || frac {
         if let Ok(f) = f64::from_str(&nstr) {
-            return Some(Number::Float(f));
+            return Ok(Number::Float(f));
         }
     } else {
         if let Ok(i) = i64::from_str(&nstr) {
-            return Some(Number::Int(i));
+            return Ok(Number::Int(i));
+        }
+    }
+    Err(JspError::InvalidNumber)
+}
+
+fn jsp_consume_low_surrogate(p: &mut PkChars) -> Option<u16> {
+    if consume_char_sequence(p, "\\u") {
+        let val = jsp_consume_four_hexdigits(p)?;
+        if val >= 0xDC00 && val <= 0xDFFF {
+            return Some(val);
         }
     }
     None
 }
 
-fn jsp_consume_low_surrogate(p: &mut PkChars) -> Option<u32> {
-    let p_clone = p.clone();
-    if consume_char_sequence(p, "\\u") {
-        if let Some(val) = jsp_consume_four_hexdigits(p) {
-            if val >= 0xDC00 && val <= 0xDFFF {
-                return Some(val);
+fn read_escaped_char(p: &mut PkChars) -> Result<char, JspError> {
+    if !consume_char(p, '\\') {
+        return Err(JspError::InvalidEscapeSequence);
+    }
+    let &c = p.peek().ok_or(JspError::Eof)?;
+    match c {
+        '"' | '\\' | '/' => {
+            p.next();
+            Ok(c)
+        }
+        'b' => {
+            p.next();
+            Ok('\x08')
+        }
+        'f' => {
+            p.next();
+            Ok('\x0C')
+        }
+        'n' => {
+            p.next();
+            Ok('\n')
+        }
+        'r' => {
+            p.next();
+            Ok('\r')
+        }
+        't' => {
+            p.next();
+            Ok('\t')
+        }
+        'u' => {
+            p.next();
+            let val = jsp_consume_four_hexdigits(p).ok_or(JspError::InvalidEscapeSequence)?;
+            match val {
+                0xDC00..=0xDFFF => {
+                    // Error: Low surrogate without a high surrogate
+                    return Err(JspError::InvalidEscapeSequence);
+                }
+                0xD800..=0xDBFF => {
+                    // val is high surrogate.
+                    let lval =
+                        jsp_consume_low_surrogate(p).ok_or(JspError::InvalidEscapeSequence)?;
+                    match char::decode_utf16([val, lval]).next().unwrap() {
+                        Ok(c) => return Ok(c),
+                        Err(e) => {
+                            println!("Error: {e}");
+                            Err(JspError::InvalidString)
+                        }
+                    }
+                }
+                _ => Ok(char::from_u32(val as u32).unwrap()),
             }
         }
+        /* Invalid escape sequence */
+        _ => Err(JspError::InvalidString),
     }
-    p.clone_from(&p_clone);
-    None
 }
 
 /** Consume a JSON string */
-pub fn jsp_consume_string(p: &mut PkChars) -> Option<String> {
-    let p_clone = p.clone();
+pub fn jsp_consume_string(p: &mut PkChars) -> Result<String, JspError> {
     let mut s = String::new();
     if !consume_char(p, '"') {
-        p.clone_from(&p_clone);
-        return None;
+        return Err(JspError::InvalidString);
     }
-    let mut escaped = false;
     loop {
         if let Some(&c) = p.peek() {
             if c.is_ascii_control() && c != '\x7F' {
-                p.clone_from(&p_clone);
-                return None;
-            }
-            if escaped {
-                match c {
-                    '"' | '\\' | '/' => {
-                        s.push(c);
-                        p.next();
-                    }
-                    'b' => {
-                        s.push('\x08');
-                        p.next();
-                    }
-                    'f' => {
-                        s.push('\x0C');
-                        p.next();
-                    }
-                    'n' => {
-                        s.push('\n');
-                        p.next();
-                    }
-                    'r' => {
-                        s.push('\r');
-                        p.next();
-                    }
-                    't' => {
-                        s.push('\t');
-                        p.next();
-                    }
-                    'u' => {
-                        p.next();
-                        if let Some(val) = jsp_consume_four_hexdigits(p) {
-                            if val >= 0xDC00 && val <= 0xDFFF {
-                                // Error: Low surrogate without a high surrogate
-                                p.clone_from(&p_clone);
-                                return None;
-                            }
-                            if val >= 0xD800 && val <= 0xDBFF {
-                                // val is high surrogate.
-                                if let Some(lval) = jsp_consume_low_surrogate(p) {
-                                    let v: [u16; 2] =
-                                        [val.try_into().unwrap(), lval.try_into().unwrap()];
-                                    for x in char::decode_utf16(v) {
-                                        match x {
-                                            Ok(c) => {
-                                                s.push(c);
-                                            }
-                                            _ => {
-                                                p.clone_from(&p_clone);
-                                                return None;
-                                            }
-                                        }
-                                    }
-                                } else {
-                                    // Error: Unpaired surrogate
-                                    p.clone_from(&p_clone);
-                                    return None;
-                                }
-                            } else {
-                                s.push(char::from_u32(val).unwrap());
-                            }
-                        } else {
-                            p.clone_from(&p_clone);
-                            return None;
-                        }
-                    }
-                    /* Invalid escape sequence */
-                    _ => {
-                        p.clone_from(&p_clone);
-                        return None;
-                    }
-                }
-                escaped = false;
-                continue;
+                return Err(JspError::InvalidString);
             }
             match c {
                 '"' => {
                     p.next();
-                    return Some(s);
+                    return Ok(s);
                 }
                 '\\' => {
-                    escaped = true;
+                    s.push(read_escaped_char(p)?);
                 }
                 _ => {
                     s.push(c);
+                    p.next();
                 }
             }
-            p.next();
         } else {
-            p.clone_from(&p_clone);
-            return None;
+            return Err(JspError::InvalidString);
         }
     }
 }
@@ -496,7 +495,8 @@ mod tests {
             r#""\uABCD""#,
         ];
         for s in test_strs {
-            assert!(jsp_consume_string(&mut s.chars().peekable()).is_some());
+            println!("test string: {s}");
+            assert!(jsp_consume_string(&mut s.chars().peekable()).is_ok());
         }
     }
     #[test]
@@ -517,15 +517,15 @@ mod tests {
         for t in x {
             println!("test: {t}");
             let z = jsp_consume_number(&mut t.chars().peekable());
-            assert!(z.is_some());
+            assert!(z.is_ok());
         }
         for t in y {
             println!("test: {t}");
             let z = jsp_consume_number(&mut t.chars().peekable());
-            assert!(z.is_none());
+            assert!(z.is_err());
         }
         let x = "+123";
-        assert!(jsp_consume_number(&mut x.chars().peekable()).is_none());
+        assert!(jsp_consume_number(&mut x.chars().peekable()).is_err());
         let x = "0.1e-2";
         assert_eq!(
             jsp_consume_number(&mut x.chars().peekable()).unwrap(),
@@ -544,6 +544,7 @@ mod tests {
         let x = "22";
         assert_eq!(jsp_parse_json(x).unwrap().to_string(), "22");
         let x = "0.2E+03";
+        println!("test input: {}", x);
         assert_eq!(
             jsp_parse_json(x).unwrap().to_string(),
             f64::from_str(x).unwrap().to_string()
@@ -554,6 +555,7 @@ mod tests {
             JsonValue::Array(vec![]),
             JsonValue::Object(HashMap::new()),
         ];
+        println!("test input: {}", x);
         let x = jsp_parse_json(x).unwrap();
         println!("Array = {}", x);
         assert_eq!(x, JsonValue::Array(y));
@@ -562,6 +564,7 @@ mod tests {
             ("Name".to_string(), JsonValue::String("Balaji".to_string())),
             ("Age".to_string(), JsonValue::Int(40)),
         ]);
+        println!("test input: {}", x);
         let x = jsp_parse_json(x).unwrap();
         println!("Object = {}", x);
         assert_eq!(x, JsonValue::Object(y));
